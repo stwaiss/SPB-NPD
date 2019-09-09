3379448784d56ae300dbb4e64fb13914be60cfeb
Sean T Waiss <stwaiss@uwm.edu>
2019-09-09 13:14:16 -0500

    Working Employee Add

--- main.py
+++ main.py
@@ -17,12 +17,6 @@ from google.appengine.ext import ndb
 from Employee import Employee
 
 
-
-
-
-
-
-
 JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
@@ -47,6 +41,7 @@ class MainHandler(webapp2.RequestHandler):
         }
         template = JINJA_ENVIRONMENT.get_template('HTML/Login.html')
         self.response.write(template.render(values))
+
 
 class LoginHandler(webapp2.RequestHandler):
     def post(self):
@@ -204,10 +199,174 @@ class HomeHandler(webapp2.RequestHandler):
 
         # if cookie is correct, render page
         if len(employees) != 0:
+
+            values = {
+                'username':name,
+                'employeeUsername':name
+            }
+
             template = JINJA_ENVIRONMENT.get_template('HTML/Home.html')
+            self.response.write(template.render(values))
         else:
             self.redirect('/')
             return
+
+
+class AdminManageEmployeeHandler(webapp2.RequestHandler):
+    def get(self):
+        # check for correct cookie
+        name = self.request.cookies.get("name")
+        admins = Employee.query(Employee.username == name, Employee.isAdmin == True).fetch()
+
+        # if cookie is correct, render page
+        if len(admins) != 0:
+            employees = Employee.query().order(Employee.username).fetch()
+
+            values = {
+                "username": name,
+                "employee": employees,
+                "reset": 0,
+                "employeeUsername": ""
+            }
+            template = JINJA_ENVIRONMENT.get_template('HTML/AdminManageEmployees.html')
+            self.response.write(template.render(values))
+        else:
+            self.redirect('/')
+            return
+
+
+class AdminManageEmployeeCreateHandler(webapp2.RequestHandler):
+    def get(self):
+        # check for correct cookie
+        name = self.request.cookies.get("name")
+        admins = Employee.query(Employee.username == name, Employee.isAdmin == True).fetch()
+
+        # if cookie is correct, render page
+        if len(admins) != 0:
+            values = {
+                "username": name,
+                "alreadyExists": 0,
+            }
+            template = JINJA_ENVIRONMENT.get_template('HTML/AdminManageEmployeesCreate.html')
+            self.response.write(template.render(values))
+        else:
+            self.redirect('/')
+            return
+
+    def post(self):
+        # check for correct cookie
+        name = self.request.cookies.get("name")
+        admins = Employee.query(Employee.username == name, Employee.isAdmin == True).fetch()
+
+        # if cookie is correct, render page
+        if len(admins) != 0:
+            # pull form data
+            postedUsername = self.request.get('username')
+            postedPassword = self.request.get('password')
+            postedEmail = self.request.get('email')
+            postedIsAdminList = self.request.get('admin')
+
+            if postedIsAdminList != "":
+                postedIsAdminBool = True
+            else:
+                postedIsAdminBool = False
+
+            # check if username already exists, if yes, throw error
+            matchingUsernames = Employee.query(Employee.username == postedUsername).fetch()
+
+            if len(matchingUsernames) != 0:
+                values = {
+                    "username": name,
+                    "alreadyExists": 1,
+                }
+                template = JINJA_ENVIRONMENT.get_template('HTML/AdminManageEmployeesCreate.html')
+                self.response.write(template.render(values))
+                return
+
+            # create employee and add to datastore
+            newEmployee = Employee(username=postedUsername, password=postedPassword, email=postedEmail, isAdmin=postedIsAdminBool, isActive=True)
+            newEmployee.put()
+
+            values = {
+                "username": name,
+                "employee": newEmployee
+            }
+
+            template = JINJA_ENVIRONMENT.get_template('HTML/AdminManageEmployeesCreateSuccess.html')
+            self.response.write(template.render(values))
+
+        else:
+            self.redirect('/')
+            return
+
+
+class AdminManageEmployeeActivateHandler(webapp2.RequestHandler):
+    def post(self):
+        # check for correct cookie
+        name = self.request.cookies.get("name")
+        admins = Employee.query(Employee.username == name, Employee.isAdmin == True).fetch()
+
+        # if cookie is correct, render page
+        if len(admins) != 0:
+            # get employee key from page and turn back into employee entity
+            employee_key_string = self.request.get('employee_key')
+            employee_key = ndb.Key(urlsafe=employee_key_string)
+            employee = employee_key.get()
+
+            # if is true, set false, vice versa
+            if employee.isActive:
+                employee.isActive = False
+
+            elif not employee.isActive:
+                employee.isActive = True
+
+            employee.put()
+
+            employees = Employee.query().order(Employee.isAdmin,Employee.username).fetch()
+
+            values = {
+                "username": name,
+                "employee": employees
+            }
+            template = JINJA_ENVIRONMENT.get_template('HTML/AdminManageEmployees.html')
+            self.response.write(template.render(values))
+        else:
+            self.redirect('/')
+            return
+
+
+class AdminManageEmployeeResetHandler(webapp2.RequestHandler):
+    def post(self):
+        # check for correct cookie
+        name = self.request.cookies.get("name")
+        admins = Employee.query(Employee.username == name, Employee.isAdmin == True).fetch()
+
+        # if cookie is correct, render page
+        if len(admins) != 0:
+            # get employee key from page and turn back into employee entity
+            employee_key_string = self.request.get('employee_key')
+            employee_key = ndb.Key(urlsafe=employee_key_string)
+            employee = employee_key.get()
+
+            # reset password
+            employee.password = "password"
+            employee.put()
+
+            # pull list for table
+            employees = Employee.query().order(Employee.isAdmin, Employee.username).fetch()
+
+            values = {
+                "username": name,
+                "employee": employees,
+                "reset": 1,
+                "employeeUsername": employee.username
+            }
+            template = JINJA_ENVIRONMENT.get_template('HTML/AdminManageEmployees.html')
+            self.response.write(template.render(values))
+        else:
+            self.redirect('/')
+            return
+
 
 
 app = webapp2.WSGIApplication([
@@ -215,5 +374,9 @@ app = webapp2.WSGIApplication([
     ('/login', LoginHandler),
     ('/logout', LogoutHandler),
     ('/changePassword', ChangePasswordHandler),
-    ('/home', HomeHandler)
+    ('/home', HomeHandler),
+    ('/admin/manageEmployee', AdminManageEmployeeHandler),
+    ('/admin/manageEmployee/create', AdminManageEmployeeCreateHandler),
+    ('/admin/manageEmployee/activate', AdminManageEmployeeActivateHandler),
+    ('/admin/manageEmployee/reset', AdminManageEmployeeResetHandler)
 ], debug=True)
